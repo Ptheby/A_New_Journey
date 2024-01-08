b682ced9b0e4b246a2d37e798abff0bb11423c10
Paul Theby <paultheby@gmail.com>
2024-01-07 20:45:15 -0600

    Practice Questions at the End of the first slide

--- practice_problems.rb
+++ practice_problems.rb
@@ -56,4 +56,165 @@ describe 'multiply' do
   end
 end
 #success!
-  
+
+#6. Arrays
+#Create a Ruby script that iterates over the array [1, 2, 3, 4, 5] and prints each element.
+
+[1,2,3,4,5].each do |num|
+    puts num
+  end
+#1 2 3 4 5
+
+#7. Hashes
+#Create a Ruby script that iterates over the hash { "name" => "Alice", "age" => 30 } and prints each key-value pair.
+
+my_hash={"name"=>"Alice", "age"=>30}
+my_hash.each do |key,value|
+  puts "#{key}:#{value}"
+end
+#  name:Alice  age:30
+
+
+#8. Loops & Control Flow
+#Create a Ruby script that prints the numbers from 1 to 20. For multiples of three, print "Fizz" instead of the number. For multiples of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz".
+(1..20).each do |num|   #must be () and not [] to do .. operator on numbers. 
+    if num % 3 == 0 && num % 5 ==0
+      puts 'FizzBuzz'
+    elsif num % 3 == 0 
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end  #must have two ends here because two methods are used   an each and elsif
+  #  12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19Buzz
+
+  #9. Super Fizz Buzz
+#Create a Ruby script that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number. For multiples of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz". For numbers that are multiples of seven, print "Super". For numbers that are multiples of both three and seven, print "FizzSuper". For numbers that are multiples of both five and seven, print "BuzzSuper". For numbers that are multiples of three, five, and seven, print "FizzBuzzSuper".
+
+(1..105).each do |num|
+    if num % 3 == 0 && num % 5 ==0 && num % 7 ==0
+      puts "FizzBuzzSuper"
+    elsif num % 3 == 0 && num % 5 ==0
+      puts 'FizzBuzz'
+    elsif num % 3 ==0 && num % 7 ==0
+      puts "FizzSuper"
+    elsif num % 5 ==0 && num % 7 ==0
+      puts "BuzzSuper"
+  elsif num % 3 == 0 
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    elsif num % 7 == 0
+      puts "Super"
+    else
+      puts num
+    end
+  end
+
+  #1
+2
+Fizz
+4
+Buzz
+Fizz
+Super
+8
+Fizz
+Buzz
+11
+Fizz
+13
+Super
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+FizzSuper
+22
+23
+Fizz
+Buzz
+26
+Fizz
+Super
+29
+FizzBuzz
+31
+32
+Fizz
+34
+BuzzSuper
+Fizz
+37
+38
+Fizz
+Buzz
+41
+FizzSuper
+43
+44
+FizzBuzz
+46
+47
+Fizz
+Super
+Buzz
+Fizz
+52
+53
+Fizz
+Buzz
+Super
+Fizz
+58
+59
+FizzBuzz
+61
+62
+FizzSuper
+64
+Buzz
+Fizz
+67
+68
+Fizz
+BuzzSuper
+71
+Fizz
+73
+74
+FizzBuzz
+76
+Super
+Fizz
+79
+Buzz
+Fizz
+82
+83
+FizzSuper
+Buzz
+86
+Fizz
+88
+89
+FizzBuzz
+Super
+92
+Fizz
+94
+Buzz
+Fizz
+97
+Super
+Fizz
+Buzz
+101
+Fizz
+103
+104
+FizzBuzzSuper
